c56045ac3bbc5b47574feab2efafc9e461655adb
Viktoria <zaharova181114@gmail.com>
2021-03-18 18:47:45 +0200

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -21,8 +21,8 @@ var categorySlider = $('.project-category-slider');
 
 $('.project-category-slider-counter .default').text("/ " + categorySlider.slick("getSlick").slideCount);
 
-categorySlider.on('afterChange', function(event, slick, currentSlide){
-    $(".project-category-slider-counter .cp").text(currentSlide<10?`${currentSlide+1}`:currentSlide+1);
+categorySlider.on('afterChange', function (event, slick, currentSlide) {
+    $(".project-category-slider-counter .cp").text(currentSlide < 10 ? `${currentSlide + 1}` : currentSlide + 1);
 });
 
 $('.project-slider').slick({
@@ -42,8 +42,8 @@ var projectSlider = $('.project-slider');
 
 $('.project-slider-counter .default').text("/ " + projectSlider.slick("getSlick").slideCount);
 
-projectSlider.on('afterChange', function(event, slick, currentSlide){
-    $(".project-slider-counter .cp").text(currentSlide<10?`${currentSlide+1}`:currentSlide+1);
+projectSlider.on('afterChange', function (event, slick, currentSlide) {
+    $(".project-slider-counter .cp").text(currentSlide < 10 ? `${currentSlide + 1}` : currentSlide + 1);
 });
 
 $('.reviews-slider').slick({
@@ -58,6 +58,26 @@ $('.reviews-slider').slick({
         '  <path d="M1088.99,4840.99l11.37-22.99-11.37-23h9.65l11.37,23-11.37,22.99h-9.65Z" transform="translate(-1089 -4795)"/>\n' +
         '</svg>\n</button>'
 });
+
+// slick active
+$(window).on('load resize', function() {
+    if ($(window).width() < 576) {
+        $('.why-slider:not(.slick-initialized)').slick({
+            infinite: false,
+            slidesToShow: 2,
+            prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="46" viewBox="0 0 21 46">\n' +
+                '    <path d="M371.007,4840.99L359.638,4818l11.369-23h-9.644l-11.37,23,11.37,22.99h9.644Z"\n' +
+                '          transform="translate(-350 -4795)"/>\n' +
+                '</svg>\n</button>',
+            nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="46" viewBox="0 0 21 46">\n' +
+                '  <path d="M1088.99,4840.99l11.37-22.99-11.37-23h9.65l11.37,23-11.37,22.99h-9.65Z" transform="translate(-1089 -4795)"/>\n' +
+                '</svg>\n</button>'
+        });
+    } else {
+        $(".why-slider.slick-initialized").slick("unslick");
+    }
+});
+// slick active
 
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
@@ -74,3 +94,12 @@ $(document).ready(function () {
     });
 });
 //плавный скролл end
+
+// mobile menu
+$('.btn-burger').on('click', function () {
+    $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.mobile-menu').fadeOut();
+});
